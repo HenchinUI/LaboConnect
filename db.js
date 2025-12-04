b6382be6680104fb0acd9e89a44c7a35c799a937
HenchinUI <harjimilan4@gmail.com>
2025-12-05 03:56:24 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
-// db.js
-const { Pool } = require("pg");
+
+
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "postgresql://db_4c25_user:Oazer3XocCByWewoTZ6Xdo9uqquUYxtH@dpg-d4oou9idbo4c73f984rg-a.singapore-postgres.render.com/db_4c25",
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.postgresql://db_4c25_user:Oazer3XocCByWewoTZ6Xdo9uqquUYxtH@dpg-d4oou9idbo4c73f984rg-a/db_4c25,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
