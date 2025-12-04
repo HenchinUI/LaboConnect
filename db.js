f01f897041f71c7fb5404bd81f60b76855b5c6ff
HenchinUI <harjimilan4@gmail.com>
2025-12-05 04:05:43 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
-
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.postgresql://db_4c25_user:Oazer3XocCByWewoTZ6Xdo9uqquUYxtH@dpg-d4oou9idbo4c73f984rg-a/db_4c25,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
